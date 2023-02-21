beaccbdad6c644129bb90ad9df3280b10288cd1f
Jorge Andrade <jandrade@i-t-m.com>
2023-02-20 19:16:46 -0600

    Test PJ1

---
+++ pj1.ps1
@@ -0,0 +1,3 @@
+Get-ADOrganizationalUnit -Filter 'Name -like "Gerencia| Format-Table Name, DistinguishedName -A
+Get-ADOrganizationalUnit -Filter 'Name -like "Compras| Format-Table Name, DistinguishedName -A
+Get-ADOrganizationalUnit -Filter 'Name -like "Ventas| Format-Table Name, DistinguishedName -A
